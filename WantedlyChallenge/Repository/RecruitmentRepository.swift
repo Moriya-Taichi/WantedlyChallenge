017c2535f342d27a30d36237b424a8660b9fc36b
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-20 08:26:49 +0900

    add service and repository file

---
+++ WantedlyChallenge/Repository/RecruitmentRepository.swift
@@ -0,0 +1,9 @@
+//
+//  RecruitmentRepository.swift
+//  WantedlyChallenge
+//
+//  Created by Mori on 2020/07/19.
+//  Copyright © 2020 Mori. All rights reserved.
+//
+
+import Foundation
---
+++ WantedlyChallenge/Repository/WantedlyRequest.swift
@@ -0,0 +1,55 @@
+//
+//  WantedlyMoyaClient.swift
+//  WantedlyChallenge
+//
+//  Created by Mori on 2020/07/19.
+//  Copyright © 2020 Mori. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum WantedlyRequestType {
+    case nothing(Int)
+    case search((word: String, page: Int))
+}
+
+extension WantedlyRequestType: TargetType {
+    var baseURL: URL {
+        guard
+            let urlString = Bundle.main.object(forInfoDictionaryKey: "API_URL") as? String,
+            let url = URL(string: urlString) else {
+                fatalError("Loading Url failed")
+        }
+        return url
+    }
+
+    var path: String {
+        switch self {
+        case let .nothing(page):
+            return "?page\(page)"
+        case let .search(param):
+            return "?q=\(param.word)&page=\(param.page)"
+        }
+    }
+
+    var method: Moya.Method {
+        return .get
+    }
+
+    var sampleData: Data {
+        let mockData = Bundle.main.path(forResource: "mock",
+                                        ofType: "json")
+        return 
+    }
+
+    var task: Task {
+
+    }
+
+    var headers: [String : String]? {
+        return nil
+    }
+
+
+}

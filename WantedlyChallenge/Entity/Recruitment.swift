ab1966cf0635b5e90f3b40d1885adda2ce5bbcbd
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-19 16:22:37 +0900

    implement: Recruitment Entity

--- WantedlyChallenge/Entity/Recruitment.swift
+++ WantedlyChallenge/Entity/Recruitment.swift
@@ -1,5 +1,5 @@
 //
-//  Recruitting.swift
+//  Recruitment.swift
 //  WantedlyChallenge
 //
 //  Created by Mori on 2020/07/19.
@@ -12,7 +12,7 @@ struct Recruitment: Codable {
     let id: Int
     let title: String
     let description: String
-    let image: RecruitingImage
+    let image: Image
     let canSupport: Bool
     let canBookmark: Bool
     let lookingFor: String
@@ -21,40 +21,7 @@ struct Recruitment: Codable {
     let pageView: Int
     let location: String
     let locationSuffix: String
-    let staffings: [RecruitingStaff]
-    let leader: RecruitingLeader
-    let company: RecruitingCompany
+    let staffings: [Staff]
+    let leader: Leader
+    let company: Company
 }
-
-struct RecruitingImage: Codable {
-    let original: String
-}
-
-struct RecruitingCompany: Codable {
-    let id: Int
-    let name: String
-    let founder: String
-    let foundedOn: String
-    let addressPrefix: String
-    let addressSuffix: String
-    let latitude: Double
-    let longnitude: Double
-    let url: String
-    let fontColorCode: String
-    let avatar: RecruitingImage
-}
-
-struct RecruitingStaff: Codable {
-    let userId: String
-    let isLeader: Bool
-    let name: String
-    let description: String
-    let facebookUid: String?
-}
-
-struct RecruitingLeader: Codable {
-    let nameJa: String?
-    let nameEn: String?
-    let facebookUid: String?
-}
-

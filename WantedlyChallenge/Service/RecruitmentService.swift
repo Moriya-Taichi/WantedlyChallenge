4c43818e40c4204e29409b0fd0b4fe808bfb0031
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-20 08:32:26 +0900

    implement: service type and service

--- WantedlyChallenge/Service/RecruitmentService.swift
+++ WantedlyChallenge/Service/RecruitmentService.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol RecruitmentServiceType {
+    func fetchRecruitment(page: Int) -> Observable<Page<Recruitment>>
+    func serchRecruitment(word: String, page: Int) -> Observable<Page<Recruitment>>
+}
+
+struct RecruitmentService: RecruitmentServiceType {
+
+    let repository: RecruitmentRepositoryType
+
+    init(repository: RecruitmentRepositoryType) {
+        self.repository = repository
+    }
+
+    func fetchRecruitment(page: Int) -> Observable<Page<Recruitment>> {
+        let input = WantedlyRequestType.nothing(page)
+        return repository.fetchRecruitment(input: input)
+            .map { recruitments in
+                return Page(pageNumber: page + 1, collection: recruitments)
+            }
+            .asObservable()
+    }
+
+    func serchRecruitment(word: String, page: Int)
+        -> Observable<Page<Recruitment>> {
+            let input = WantedlyRequestType.search((word: word, page: page))
+            return repository.fetchRecruitment(input: input)
+                .map { recruitments in
+                    return Page(pageNumber: page + 1, collection: recruitments)
+                }
+                .asObservable()
+    }
+}

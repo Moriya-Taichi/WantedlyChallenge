e49cf71190f0a1a13bdd82c18de9d3d84b31b8ea
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-20 12:24:49 +0900

    implement: mutation logic

--- WantedlyChallenge/Reactor/RecrutingCatalog/RecrutingCatalogViewReactor.swift
+++ WantedlyChallenge/Reactor/RecrutingCatalog/RecrutingCatalogViewReactor.swift
@@ -13,25 +13,67 @@ final class RecrutingCatalogViewReactor: Reactor {
 
     enum Action {
         case load
-        case search(String)
-        case paginate
+        case search(String?)
+        case paginate(String?)
     }
 
     enum Mutation {
-        case setRecrutings([Recruiting])
-        case setPageNumber(Int)
+        case setRecruitments(Page<CellItem>)
         case setIsLoading(Bool)
     }
 
     struct State {
+        var page: Page<CellItem>
         var isLoading: Bool
     }
 
     var initialState: State
+    private let recruitmentService: RecruitmentServiceType
 
-    private var pageNumber = 0
+    init(recruitmentService: RecruitmentServiceType) {
+        initialState = State(page: .init(pageNumber: 0,
+                                         collection: []),
+                             isLoading: false)
+        self.recruitmentService = recruitmentService
+    }
 
-    init() {
-        initialState = State(isLoading: false)
+    func mutate(action: Action) -> Observable<Mutation> {
+        let startLoading: Observable<Mutation> = .just(.setIsLoading(true))
+        let endLoading: Observable<Mutation> = .just(.setIsLoading(false))
+        switch action {
+        case .load:
+            let rectuitment = recruitmentService
+                .fetchRecruitment(page: 0)
+                .map { $0.map(CellItem.recruitmentCellItem) }
+                .map(Mutation.setRecruitments)
+            return .concat([startLoading, rectuitment, endLoading])
+        case let .search(word):
+            guard let word = word, !word.isEmpty else {
+                self.action.onNext(.load)
+                return .empty()
+            }
+            let serchedRecruitments = recruitmentService
+                .serchRecruitment(word: word,
+                                  page: 0)
+                .map { $0.map(CellItem.recruitmentCellItem) }
+                .map(Mutation.setRecruitments)
+            return .concat([startLoading, serchedRecruitments, endLoading])
+        case let .paginate(word):
+            guard let word = word, !word.isEmpty else {
+                let recruitmentPagination = recruitmentService
+                    .fetchRecruitment(page: currentState.page.pageNumber)
+                    .map { $0.map(CellItem.recruitmentCellItem) }
+                    .map { self.currentState.page.paginate($0) }
+                    .map(Mutation.setRecruitments)
+                return .concat([startLoading, recruitmentPagination, endLoading])
+            }
+            let serchPagenation = recruitmentService
+                .serchRecruitment(word: word,
+                                  page: currentState.page.pageNumber)
+                .map { $0.map(CellItem.recruitmentCellItem) }
+                .map { self.currentState.page.paginate($0) }
+                .map(Mutation.setRecruitments)
+            return .concat([startLoading, serchPagenation, endLoading])
+        }
     }
 }

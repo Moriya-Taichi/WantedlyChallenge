85e2e0d59588a7629bfd1069ada32b91f8267dfc
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-20 12:23:30 +0900

    implement: reach bottom event

--- WantedlyChallenge/Extension/UIScrollView+Rx.swift
+++ WantedlyChallenge/Extension/UIScrollView+Rx.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+extension Reactive where Base: UIScrollView {
+    var isReachedBottom: Observable<Void> {
+        let source = self.contentOffset
+            .filter { [weak base] offset -> Bool in
+                guard let base = base else { return false }
+                let offsetToBottom = base.contentOffset.y + base.bounds.height
+                return offsetToBottom >= base.contentSize.height - base.bounds.height / 3
+            }
+            .map { _ in Void() }
+        return source
+    }
+}

94d8d64ff2f50d39f3087182c73edc7a510c520d
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-20 12:17:56 +0900

    implement: ViewController Lifecycle + RX

--- WantedlyChallenge/Extension/UIViewController+Rx.swift
+++ WantedlyChallenge/Extension/UIViewController+Rx.swift
@@ -1,9 +1,19 @@
 //
-//  UIViewController+Rx.swift
-//  WantedlyChallenge
+//  UIViewController+Reactive.swift
 //
-//  Created by Mori on 2020/07/20.
-//  Copyright © 2020 Mori. All rights reserved.
+//  Created by Suguru Kishimoto on 2016/12/06.
+//  reference: https://qiita.com/sgr-ksmt/items/e259e00f5c0a2f3109ff
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
+import UIKit
+
+extension Reactive where Base: UIViewController {
+    public var viewDidLoad: Observable<Void> {
+        return sentMessage(#selector(Base.viewDidLoad))
+            .map { _ in }
+            .share(replay: 1)
+    }
+}
+

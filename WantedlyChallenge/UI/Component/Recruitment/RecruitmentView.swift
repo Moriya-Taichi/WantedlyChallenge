918a06ffcbc2b9a9f9ad86e5f2cabae25f951494
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-21 21:24:03 +0900

    implement: base load action

--- WantedlyChallenge/UI/Component/Recruitment/RecruitmentView.swift
+++ WantedlyChallenge/UI/Component/Recruitment/RecruitmentView.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+import UIKit
+
+final class RecruitmentView: UIView {
+    
+    var disposeBag = DisposeBag()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadXib()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadXib()
+    }
+}
+
+extension RecruitmentView: StoryboardView {
+    func bind(reactor: RecruitmentViewReactor) {
+        Observable<Void>.just(())
+            .map { _ in Reactor.Action.load }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+}

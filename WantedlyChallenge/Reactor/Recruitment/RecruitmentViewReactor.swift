6c40bd2bebe0b93f8c2c8245e1a5a0656f51f8b9
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-21 21:22:12 +0900

    implement: base load action

--- WantedlyChallenge/Reactor/Recruitment/RecruitmentViewReactor.swift
+++ WantedlyChallenge/Reactor/Recruitment/RecruitmentViewReactor.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class RecruitmentViewReactor: Reactor {
+
+    enum Action {
+        case load
+    }
+
+    enum Mutation {
+        case setRecruitment(Recruitment)
+    }
+
+    struct State {
+        var recruitment: Recruitment?
+    }
+
+    var initialState: State
+    private let recruitmentId: Int
+    private let recruitmentService: RecruitmentServiceType
+
+    init(recruitmentId: Int, recruitmentService: RecruitmentServiceType) {
+        initialState = State(recruitment: nil)
+        self.recruitmentId = recruitmentId
+        self.recruitmentService = recruitmentService
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            return recruitmentService.fetchRecruitment(id: recruitmentId)
+                .map(Mutation.setRecruitment)
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .setRecruitment(recruitment):
+            newState.recruitment = recruitment
+        }
+        return newState
+    }
+}

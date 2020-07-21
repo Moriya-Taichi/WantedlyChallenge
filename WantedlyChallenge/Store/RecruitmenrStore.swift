a2da568a638480beadd91419fe93fbf3ddffa2f5
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-21 21:08:49 +0900

    implement: store recruitment

--- WantedlyChallenge/Store/RecruitmenrStore.swift
+++ WantedlyChallenge/Store/RecruitmenrStore.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+protocol RecruitmentStoreType {
+    func store(recruitments: [Recruitment])
+    func load(id: Int) -> Recruitment?
+    func clear()
+}
+
+final class RecruitmentStore: RecruitmentStoreType {
+
+    private var storeDictionary: [String: [Recruitment]] = [:]
+
+    func store(recruitments: [Recruitment]) {
+        if let storedRecruitments = storeDictionary["Recruitment"] {
+            storeDictionary["Recruitment"] = storedRecruitments + recruitments
+        } else {
+            storeDictionary["Recruitment"] = recruitments
+        }
+    }
+
+    func load(id: Int) -> Recruitment? {
+        guard let recruitments = storeDictionary["Recruitment"] else {
+            return nil
+        }
+        return recruitments.first { recruitment -> Bool in
+            return recruitment.id == id
+        }
+    }
+
+    func clear() {
+        storeDictionary = [:]
+    }
+
+}

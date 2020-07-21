0e5b04b0a9a0c1c02aa2d8599b3a28853a093983
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-21 15:48:30 +0900

    implement: bind state and button feedback

--- WantedlyChallenge/UI/Component/View/RecruitmentCellView/RecruitmentCellView.swift
+++ WantedlyChallenge/UI/Component/View/RecruitmentCellView/RecruitmentCellView.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import UIKit
+import ReactorKit
 import SDWebImage
+import RxOptional
+import RxSwift
+import UIKit
 
 final class RecruitmentCellView: UIView {
 
     @IBOutlet private weak var recruitImageView: UIImageView!
+    @IBOutlet weak var bookmarkButton: UIButton!
 
     @IBOutlet private weak var lookingForLabel: UILabel!
     @IBOutlet private weak var titleLabel: UILabel!
@@ -22,6 +26,11 @@ final class RecruitmentCellView: UIView {
         }
     }
     @IBOutlet private weak var companyNameLabel: UILabel!
+
+    var disposeBag = DisposeBag()
+    let mediumFeedBackGenerator = UIImpactFeedbackGenerator(style: .medium)
+    let lightFeedbackgGenerator = UIImpactFeedbackGenerator(style: .light)
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadXib()
@@ -42,4 +51,37 @@ final class RecruitmentCellView: UIView {
         recruitImageView.sd_setImage(with: URL(string: recruitment.image.original))
     }
 
+}
+
+extension RecruitmentCellView: StoryboardView {
+    func bind(reactor: RecruitmentCellViewReactor) {
+        reactor.state.map { $0.recruitment }
+            .distinctUntilChanged()
+            .subscribe(onNext: { [weak self] recruitment in
+                self?.setContents(recruitment: recruitment)
+            })
+            .disposed(by: disposeBag)
+
+        reactor.state.map { $0.isBookmark }
+            .distinctUntilChanged()
+            .bind(to: bookmarkButton.rx.isSelected)
+            .disposed(by: disposeBag)
+
+        bookmarkButton.rx.tap
+            .do(onNext: {[weak self] _ in
+                guard let self = self else {
+                    return
+                }
+                if self.bookmarkButton.isSelected {
+                    self.lightFeedbackgGenerator.prepare()
+                    self.lightFeedbackgGenerator.impactOccurred()
+                } else {
+                    self.mediumFeedBackGenerator.prepare()
+                    self.mediumFeedBackGenerator.impactOccurred()
+                }
+            })
+            .map{ _ in Reactor.Action.bookmark }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
 }

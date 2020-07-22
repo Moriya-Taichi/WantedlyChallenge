1a952feb240826d630f0bb5dc884259d91535774
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-22 23:42:29 +0900

    implement: select staff

--- WantedlyChallenge/Reactor/Recruitment/RecruitmentViewReactor.swift
+++ WantedlyChallenge/Reactor/Recruitment/RecruitmentViewReactor.swift
@@ -14,16 +14,19 @@ final class RecruitmentViewReactor: Reactor {
     enum Action {
         case load
         case bookmark
+        case selectStaff(Int)
     }
 
     enum Mutation {
         case setRecruitment(Recruitment)
         case setIsBookmark(Bool)
+        case setStaff(Staff)
     }
 
     struct State {
         var recruitment: Recruitment?
         var isBookmark: Bool
+        var displayStaff: Staff?
     }
 
     var initialState: State
@@ -31,7 +34,7 @@ final class RecruitmentViewReactor: Reactor {
     private let recruitmentService: RecruitmentServiceType
 
     init(id: Int, recruitmentService: RecruitmentServiceType) {
-        initialState = State(recruitment: nil, isBookmark: false)
+        initialState = State(recruitment: nil, isBookmark: false, displayStaff: nil)
         self.recruitmentId = id
         self.recruitmentService = recruitmentService
     }
@@ -41,12 +44,23 @@ final class RecruitmentViewReactor: Reactor {
         case .load:
             let recruitment = recruitmentService.fetchRecruitment(id: recruitmentId)
                 .flatMap { recruitment -> Observable<Mutation> in
+                    if let staff = recruitment.staffings.first {
+                        return .concat([.just(.setRecruitment(recruitment)),
+                                        .just(.setIsBookmark(recruitment.canBookmark)),
+                                        .just(.setStaff(staff))])
+                    }
                     return .concat([.just(.setRecruitment(recruitment)),
                                     .just(.setIsBookmark(recruitment.canBookmark))])
-                }
+            }
             return recruitment
         case .bookmark:
             return .just(.setIsBookmark(!currentState.isBookmark))
+        case let .selectStaff(index):
+            guard
+                let recruitment = currentState.recruitment,
+                index < recruitment.staffings.count
+                else { return .empty() }
+            return .just(.setStaff(recruitment.staffings[index]))
         }
     }
 
@@ -57,6 +71,8 @@ final class RecruitmentViewReactor: Reactor {
             newState.recruitment = recruitment
         case let .setIsBookmark(isBookamerk):
             newState.isBookmark = isBookamerk
+        case let .setStaff(staff):
+            newState.displayStaff = staff
         }
         return newState
     }

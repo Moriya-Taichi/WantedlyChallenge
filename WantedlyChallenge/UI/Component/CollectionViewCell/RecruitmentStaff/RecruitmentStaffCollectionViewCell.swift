c18c648ceb465bae83eccc662231692a1eac4b27
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-22 23:28:56 +0900

    add staff cell

---
+++ WantedlyChallenge/UI/Component/CollectionViewCell/RecruitmentStaff/RecruitmentStaffCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  RecruitmentStaffCollectionViewCell.swift
+//  WantedlyChallenge
+//
+//  Created by Mori on 2020/07/22.
+//  Copyright © 2020 Mori. All rights reserved.
+//
+
+import Foundation

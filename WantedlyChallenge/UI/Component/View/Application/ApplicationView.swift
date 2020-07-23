109418f0829d00701e8a55f57fbe9f997d97ec8d
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-23 17:21:35 +0900

    implement: view and transition

--- WantedlyChallenge/UI/Component/View/Application/ApplicationView.swift
+++ WantedlyChallenge/UI/Component/View/Application/ApplicationView.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+import UIKit
+
+final class ApplicationView: UIView {
+
+    @IBOutlet private weak var choicesTableView: UITableView!
+    @IBOutlet private weak var applicateButton: UIButton! {
+        didSet {
+            applicateButton.layer.cornerRadius = applicateButton.frame.height / 2
+        }
+    }
+    @IBOutlet private weak var backgroundButton: UIButton!
+
+    private let transitionEventSubject = PublishSubject<TransitionEvent>()
+    var transitionEventStream: Observable<TransitionEvent> {
+        return transitionEventSubject
+    }
+
+    var disposeBag: DisposeBag = .init()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadXib()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadXib()
+    }
+}
+
+extension ApplicationView: StoryboardView {
+    func bind(reactor: ApplicationViewReactor) {
+        backgroundButton.rx.tap
+            .map { _ in TransitionEvent.dismiss }
+            .bind(to: transitionEventSubject)
+            .disposed(by: disposeBag)
+    }
+}

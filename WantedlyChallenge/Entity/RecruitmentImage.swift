38c862075f898ef6280fc8425e0e1360d5683d04
Moriya-Taichi <minecraft0125@gmail.com>
2020-07-23 10:46:02 +0900

    add new entity file

---
+++ WantedlyChallenge/Entity/RecruitmentImage.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyImage.swift
+//  WantedlyChallenge
+//
+//  Created by Mori on 2020/07/23.
+//  Copyright © 2020 Mori. All rights reserved.
+//
+
+import Foundation
+
+struct CompanyImage: Codable, Hashable {
+    let original: String
+    let i320131X2: String
+    let i320131: String
+    let i304124X2: String
+    let i304124: String
+}
